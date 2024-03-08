57505153e0d3ef12a98db65bbe07833ede6e4137
4gora <rosarioalessandro03@gmail.com>
2024-03-08 00:02:10 -0300

    Continuado desenvolvimento do jogo de acordo com o projeto do vídeo

--- Game.py
+++ Game.py
@@ -5,14 +5,19 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
-text_surface = test_font.render('Meu jogo', False, 'black')
+sky_surf = pygame.image.load("graphics/Sky.png").convert()
+ground_surf = pygame.image.load("graphics/ground.png").convert()
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
-snail_x_pos = 900
+score_surf = test_font.render("Meu jogo", False, "black").convert()
+score_rect = score_surf.get_rect(center=(screen.get_width() // 2, 100))
+
+snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom=(800, 300))
+
+player_surf = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom=(80, 300))
 
 
 while True:
@@ -20,14 +25,21 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        """ if event.type == pygame.MOUSEMOTION:
+            if player_rect.collidepoint(event.pos):
+                print("Collide player") """
 
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (350, 100))
-    screen.blit(snail_surface, (snail_x_pos, 270))
+    screen.blit(sky_surf, (0, 0))
+    screen.blit(ground_surf, (0, 300))
+    screen.blit(score_surf, score_rect)
+    screen.blit(snail_surf, snail_rect)
+    screen.blit(player_surf, player_rect)
 
-    snail_x_pos -= 4
-    if snail_x_pos < -100: snail_x_pos = 900
+    snail_rect.x -= 4
+    if snail_rect.right < -0:
+        snail_rect.left = 800
 
     pygame.display.update()
     clock.tick(60)
+
+# Drawing with rectangles 1:17:44
